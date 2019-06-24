77e1732c1c4b371d85fd887c29c06562b7243924
Constantine Kutenko <constantine.kutenko@asergis.com>
2019-06-24 09:09:49 +0300

    Added evenOrOdd methond

---
+++ vars/evenOrOdd.groovy
@@ -0,0 +1,27 @@
+// evenOrOdd.groovy
+
+def call(int buildNumber) {
+    if (buildNumber % 2 == 0) {
+        pipeline {
+            agent any
+            stages {
+                stage('Even stage') {
+                    steps {
+                        echo "The build number is even"
+                    }
+                }
+            }
+        }
+    } else {
+        pipeline {
+            agent any
+            stages {
+                stage("Odd stage") {
+                    steps {
+                        echo "The build number is odd"
+                    }
+                }
+            }
+        }
+    }
+}
